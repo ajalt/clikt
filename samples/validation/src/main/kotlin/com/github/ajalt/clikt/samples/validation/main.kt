bf5787a3961761f9bdd9d0582976351fe8aa8350
AJ Alt <aj@alt>
2018-03-11 13:38:36 -0700

    Add validation example.

---
+++ samples/validation/src/main/kotlin/com/github/ajalt/clikt/samples/validation/main.kt
@@ -0,0 +1,42 @@
+package com.github.ajalt.clikt.samples.validation
+
+import com.github.ajalt.clikt.core.BadParameter
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.convert
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.validate
+import com.github.ajalt.clikt.parameters.types.int
+import java.net.MalformedURLException
+import java.net.URL
+
+class Cli : CliktCommand(help = "Validation examples") {
+    val count by option(help = "A positive even number").int()
+            .validate {
+                if (it != null && (it < 0 || it % 2 != 0)) {
+                    throw BadParameter("Should be a positive, even integer")
+                }
+            }
+    val biggerCount by option(help="A number larger than --count").int()
+    val url by option(help="A URL")
+            .convert {
+                try {
+                    URL(it)
+                } catch (err: MalformedURLException) {
+                    throw BadParameter("Invalid URL")
+                }
+            }
+
+    override fun run() {
+        // You can't refer to another parameter in a validator or converter, since the other parameter might
+        // not be set yet. Instead, validate them after parsing.
+        if (biggerCount != null && count != null && biggerCount!! <= count!!) {
+            throw BadParameter("--bigger-count must be larger than --count")
+        }
+        println("count: $count")
+        println("biggerCount: $biggerCount")
+        println("url: $url")
+    }
+
+}
+
+fun main(args: Array<String>) = Cli().main(args)

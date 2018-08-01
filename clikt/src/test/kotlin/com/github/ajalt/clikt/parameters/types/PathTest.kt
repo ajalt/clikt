e7e22ad1f072deedf3853785a596185ec5d10da6
AJ Alt <aj@alt>
2018-07-31 19:11:37 -0700

    Clean up test

--- clikt/src/test/kotlin/com/github/ajalt/clikt/parameters/types/PathTest.kt
+++ clikt/src/test/kotlin/com/github/ajalt/clikt/parameters/types/PathTest.kt
@@ -2,6 +2,7 @@ package com.github.ajalt.clikt.parameters.types
 
 import com.github.ajalt.clikt.core.BadParameterValue
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.NoRunCliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.multiple
 import com.github.ajalt.clikt.parameters.options.option
@@ -9,18 +10,19 @@ import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.testing.splitArgv
 import com.google.common.jimfs.Configuration
 import com.google.common.jimfs.Jimfs
-import io.kotlintest.fail
 import io.kotlintest.shouldBe
-import java.nio.file.Files
-import java.nio.file.FileSystem
+import io.kotlintest.shouldThrow
 import org.junit.Test
+import java.nio.file.FileSystem
+import java.nio.file.Files
 
+@Suppress("unused")
 class PathTest {
     val fs: FileSystem = Jimfs.newFileSystem(Configuration.unix())
 
     @Test
     fun `paths are resolved using the provided filesystem, if any`() {
-        val command = object : CliktCommand() {
+        class C : CliktCommand() {
             val path by option("-p")
                     .path(fileSystem = fs)
                     .required()
@@ -30,12 +32,12 @@ class PathTest {
             }
         }
 
-        command.parse(splitArgv("-p/var/log/foo"))
+        C().parse(splitArgv("-p/var/log/foo"))
     }
 
     @Test
     fun `options can be paths`() {
-        val command = object : CliktCommand() {
+        class C : CliktCommand() {
             val path by option("-p")
                     .path()
                     .required()
@@ -45,12 +47,12 @@ class PathTest {
             }
         }
 
-        command.parse(splitArgv("-pfoo"))
+        C().parse(splitArgv("-pfoo"))
     }
 
     @Test
     fun `arguments can be paths`() {
-        val command = object : CliktCommand() {
+        class C : CliktCommand() {
             val paths by argument()
                     .path()
                     .multiple()
@@ -60,61 +62,44 @@ class PathTest {
             }
         }
 
-        command.parse(splitArgv("foo bar baz"))
+        C().parse(splitArgv("foo bar baz"))
     }
 
     @Test
     fun `fileOkay = false will reject files`() {
-        val command = object : CliktCommand() {
+        class C : NoRunCliktCommand() {
             val folderOnly by option("-f").path(fileOkay = false, fileSystem = fs)
-
-            override fun run() {
-            }
         }
 
         Files.createDirectory(fs.getPath("/var"))
         Files.createFile(fs.getPath("/var/foo"))
 
-        expectBadParameter("Invalid value for \"-f\": Directory \"/var/foo\" is a file.") {
-            command.parse(splitArgv("-f/var/foo"))
-        }
+        shouldThrow<BadParameterValue> {
+            C().parse(splitArgv("-f/var/foo"))
+        }.message shouldBe """Invalid value for "-f": Directory "/var/foo" is a file."""
     }
 
     @Test
     fun `folderOkay = false will reject folders`() {
-        val command = object : CliktCommand() {
+        class C : NoRunCliktCommand() {
             val fileOnly by option("-f").path(folderOkay = false, fileSystem = fs)
-
-            override fun run() {
-            }
         }
 
         Files.createDirectories(fs.getPath("/var/foo"))
 
-        expectBadParameter("Invalid value for \"-f\": File \"/var/foo\" is a directory.") {
-            command.parse(splitArgv("-f/var/foo"))
-        }
+        shouldThrow<BadParameterValue> {
+            C().parse(splitArgv("-f/var/foo"))
+        }.message shouldBe """Invalid value for "-f": File "/var/foo" is a directory."""
     }
 
-    @Test fun `exists = true will reject paths that don't exist` () {
-        val command = object : CliktCommand() {
+    @Test
+    fun `exists = true will reject paths that don't exist`() {
+        class C : NoRunCliktCommand() {
             val homeDir by option("-h").path(exists = true, fileSystem = fs)
-
-            override fun run() {
-            }
         }
 
-        expectBadParameter("Invalid value for \"-h\": Path \"/home/cli\" does not exist.") {
-            command.parse(splitArgv("-h /home/cli"))
-        }
-    }
-
-    private inline fun expectBadParameter(message: String, fn: () -> Unit) {
-        try {
-            fn()
-            fail("parse should have failed with a BadParameterValue exception")
-        } catch (e: BadParameterValue) {
-            e.message shouldBe message
-        }
+        shouldThrow<BadParameterValue> {
+            C().parse(splitArgv("-h /home/cli"))
+        }.message shouldBe """Invalid value for "-h": Path "/home/cli" does not exist."""
     }
 }

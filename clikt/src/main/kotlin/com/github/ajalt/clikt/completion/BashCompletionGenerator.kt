01c021f42c108aa4cded2b24b17c963f35126ee1
Laurent Pireyn <laurent.pireyn@pisolutions.eu>
2018-12-27 16:15:48 +0100

    Create BashCompletionGenerator

---
+++ clikt/src/main/kotlin/com/github/ajalt/clikt/completion/BashCompletionGenerator.kt
@@ -0,0 +1,14 @@
+package com.github.ajalt.clikt.completion
+
+import com.github.ajalt.clikt.core.CliktCommand
+
+/**
+ * [CompletionGenerator] for Bash.
+ *
+ * @author Laurent Pireyn
+ */
+object BashCompletionGenerator : CompletionGenerator {
+    override fun generateCompletion(command: CliktCommand): String {
+        TODO("Not implemented")
+    }
+}

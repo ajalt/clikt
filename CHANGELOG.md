fb8380a520d7ee2f0ec75d9d07b36214481f4618
AJ Alt <aj@alt>
2019-11-02 11:04:13 -0700

    Add CompletionTest

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 # Changelog
 
 ## [Unreleased]
+### Added
+- `CompletionCandidates.Fixed` now has a secondary convenience constructor that take a `vararg` of `String`s
+
 ### Changed
 - `option()` and `argument()` now take optional `completionCandidates` parameters to override how completion is generated. The constructor and `copy` functions of `OptionsWithValues` and `ProcessedArgument` have changed to support default values.
 
--- clikt/src/main/kotlin/com/github/ajalt/clikt/completion/CompletionCandidates.kt
+++ clikt/src/main/kotlin/com/github/ajalt/clikt/completion/CompletionCandidates.kt
@@ -6,12 +6,18 @@ package com.github.ajalt.clikt.completion
 sealed class CompletionCandidates {
     /** Do not autocomplete this parameter */
     object None : CompletionCandidates()
+
     /** Complete with filesystem paths */
     object Path : CompletionCandidates()
+
     /** Complete with entries in the system's hostfile */
     object Hostname : CompletionCandidates()
+
     /** Complete with usernames from the current system */
     object Username : CompletionCandidates()
+
     /** Complete the parameter with a fixed set of string */
-    data class Fixed(val candidates: Set<String>) : CompletionCandidates()
+    data class Fixed(val candidates: Set<String>) : CompletionCandidates() {
+        constructor(vararg candidates: String) : this(candidates.toSet())
+    }
 }

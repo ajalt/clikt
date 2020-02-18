d407c5e7a8173d3401b1f69a0053f17544e25d16
AJ Alt <ajalt@users.noreply.github.com>
2020-02-18 10:01:36 -0800

    Expand tildes for file and path checks (#132)

--- clikt/src/jvmTest/kotlin/com/github/ajalt/clikt/parameters/types/PathTest.kt
+++ clikt/src/jvmTest/kotlin/com/github/ajalt/clikt/parameters/types/PathTest.kt
@@ -8,14 +8,20 @@ import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.testing.TestCommand
 import com.google.common.jimfs.Configuration
 import com.google.common.jimfs.Jimfs
-import io.kotest.matchers.shouldBe
 import io.kotest.assertions.throwables.shouldThrow
-import kotlin.test.Test
+import io.kotest.matchers.shouldBe
+import org.junit.Rule
+import org.junit.contrib.java.lang.system.RestoreSystemProperties
 import java.nio.file.FileSystem
 import java.nio.file.Files
+import kotlin.test.Test
+
 
 @Suppress("unused")
 class PathTest {
+    @Rule
+    @JvmField
+    val restoreSystemProperties = RestoreSystemProperties()
     val fs: FileSystem = Jimfs.newFileSystem(Configuration.unix())
 
     @Test
@@ -99,5 +105,29 @@ class PathTest {
         shouldThrow<BadParameterValue> {
             C().parse("-h /home/cli")
         }.message shouldBe """Invalid value for "-h": Path "/home/cli" does not exist."""
+    }
+
+    @Test
+    fun `mustExist = true expands tildes internally when home is set`() {
+        val home = "/test/home/clikt"
+
+        Files.createDirectories(fs.getPath(home))
+        Files.createFile(fs.getPath(home, "foo"))
+
+        class C(called: Boolean) : TestCommand(called = called) {
+            val homeDir by option("-h").path(mustExist = true, fileSystem = fs)
+
+            override fun run_() {
+                homeDir.toString() shouldBe "~/foo"
+            }
+        }
+
+        System.setProperty("user.home", home)
+        C(true).parse("-h ~/foo")
+
+        System.clearProperty("user.home")
+        shouldThrow<BadParameterValue> {
+            C(false).parse("-h ~/foo")
+        }.message shouldBe """Invalid value for "-h": Path "~/foo" does not exist."""
     }
 }

9a1a87800fea19e1710b529785eb59a1dbf01f88
AJ <aj@alt>
2020-06-19 11:19:27 -0700

    Use snapshot version of kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,9 @@ allprojects {
     repositories {
         mavenCentral()
         jcenter()
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+        }
     }
 }
 

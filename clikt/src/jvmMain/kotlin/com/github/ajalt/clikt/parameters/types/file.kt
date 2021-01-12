b6c8b917e1b210b166a8e63d9ac52520711ef596
AJ <aj@alt>
2021-01-12 10:21:19 -0800

    Simplify file type conversions
    Rather than repeating all of the attribute checks, we now reuse the Path
    implementation

--- clikt/src/jvmMain/kotlin/com/github/ajalt/clikt/parameters/types/file.kt
+++ clikt/src/jvmMain/kotlin/com/github/ajalt/clikt/parameters/types/file.kt
@@ -1,7 +1,6 @@
 package com.github.ajalt.clikt.parameters.types
 
 import com.github.ajalt.clikt.completion.CompletionCandidates
-import com.github.ajalt.clikt.core.Context
 import com.github.ajalt.clikt.parameters.arguments.ProcessedArgument
 import com.github.ajalt.clikt.parameters.arguments.RawArgument
 import com.github.ajalt.clikt.parameters.arguments.convert
@@ -9,37 +8,7 @@ import com.github.ajalt.clikt.parameters.options.NullableOption
 import com.github.ajalt.clikt.parameters.options.RawOption
 import com.github.ajalt.clikt.parameters.options.convert
 import java.io.File
-import java.nio.file.Files
-
-private fun pathType(context: Context, fileOkay: Boolean, folderOkay: Boolean): String = when {
-    fileOkay && !folderOkay -> context.localization.pathTypeFile()
-    !fileOkay && folderOkay -> context.localization.pathTypeDirectory()
-    else -> context.localization.pathTypeOther()
-}
-
-private fun convertToFile(
-        path: String,
-        mustExist: Boolean,
-        canBeFile: Boolean,
-        canBeDir: Boolean,
-        mustBeWritable: Boolean,
-        mustBeReadable: Boolean,
-        canBeSymlink: Boolean,
-        context: Context,
-        fail: (String) -> Unit
-): File {
-    val name = pathType(context, canBeFile, canBeDir)
-    return with(context.localization) {
-        File(path).also {
-            if (mustExist && !it.exists()) fail(pathDoesNotExist(name, it.toString()))
-            if (!canBeFile && it.isFile) fail(pathIsFile(name, it.toString()))
-            if (!canBeDir && it.isDirectory) fail(pathIsDirectory(name, it.toString()))
-            if (mustBeWritable && !it.canWrite()) fail(pathIsNotWritable(name, it.toString()))
-            if (mustBeReadable && !it.canRead()) fail(pathIsNotReadable(name, it.toString()))
-            if (!canBeSymlink && Files.isSymbolicLink(it.toPath())) fail(pathIsSymlink(name, it.toString()))
-        }
-    }
-}
+import java.nio.file.FileSystems
 
 /**
  * Convert the argument to a [File].
@@ -59,8 +28,18 @@ fun RawArgument.file(
         mustBeReadable: Boolean = false,
         canBeSymlink: Boolean = true
 ): ProcessedArgument<File, File> {
-    return convert(completionCandidates = CompletionCandidates.Path) { str ->
-        convertToFile(str, mustExist, canBeFile, canBeDir, mustBeWritable, mustBeReadable, canBeSymlink, context) { fail(it) }
+    return convert(CompletionCandidates.Path) { str ->
+        convertToPath(
+            path = str,
+            mustExist = mustExist,
+            canBeFile = canBeFile,
+            canBeFolder = canBeDir,
+            mustBeWritable = mustBeWritable,
+            mustBeReadable = mustBeReadable,
+            canBeSymlink = canBeSymlink,
+            fileSystem = FileSystems.getDefault(),
+            context = context
+        ) { fail(it) }.toFile()
     }
 }
 
@@ -83,6 +62,16 @@ fun RawOption.file(
         canBeSymlink: Boolean = true
 ): NullableOption<File, File> {
     return convert({ localization.pathMetavar() }, CompletionCandidates.Path) { str ->
-        convertToFile(str, mustExist, canBeFile, canBeDir, mustBeWritable, mustBeReadable, canBeSymlink, context) { fail(it) }
+        convertToPath(
+            path = str,
+            mustExist = mustExist,
+            canBeFile = canBeFile,
+            canBeFolder = canBeDir,
+            mustBeWritable = mustBeWritable,
+            mustBeReadable = mustBeReadable,
+            canBeSymlink = canBeSymlink,
+            fileSystem = FileSystems.getDefault(),
+            context = context
+        ) { fail(it) }.toFile()
     }
 }

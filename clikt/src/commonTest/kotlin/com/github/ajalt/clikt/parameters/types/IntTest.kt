f318ae0efe9b95d1072f379ab1a842beefbfbe26
AJ <aj@alt>
2021-10-18 10:17:43 -0700

    Update int arg test

--- clikt/src/commonTest/kotlin/com/github/ajalt/clikt/parameters/types/IntTest.kt
+++ clikt/src/commonTest/kotlin/com/github/ajalt/clikt/parameters/types/IntTest.kt
@@ -83,6 +83,7 @@ class IntTypeTest {
     fun `int argument`() = forAll(
         row("", null, emptyList()),
         row("1 2", 1, listOf(2)),
+        row("-- -1 -2", -1, listOf(-2)),
         row("1 2 3", 1, listOf(2, 3))) { argv, ex, ey ->
         class C : TestCommand() {
             val x by argument().int().optional()

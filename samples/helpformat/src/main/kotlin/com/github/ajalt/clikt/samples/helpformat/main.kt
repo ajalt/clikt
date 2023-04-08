4fc726830b343e147a092e2bce4f63202763816b
AJ <aj@alt>
2023-04-08 10:58:17 -0700

    Update helpformat sample

--- samples/helpformat/src/main/kotlin/com/github/ajalt/clikt/samples/helpformat/main.kt
+++ samples/helpformat/src/main/kotlin/com/github/ajalt/clikt/samples/helpformat/main.kt
@@ -1,44 +1,48 @@
 package com.github.ajalt.clikt.samples.helpformat
 
 import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.core.UsageError
+import com.github.ajalt.clikt.core.Context
 import com.github.ajalt.clikt.core.context
-import com.github.ajalt.clikt.output.CliktHelpFormatter
 import com.github.ajalt.clikt.output.HelpFormatter
-import com.github.ajalt.clikt.output.Localization
+import com.github.ajalt.clikt.output.MordantHelpFormatter
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.multiple
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.mordant.rendering.TextAlign
+import com.github.ajalt.mordant.rendering.Widget
+import com.github.ajalt.mordant.table.ColumnWidth
+import com.github.ajalt.mordant.table.verticalLayout
+import com.github.ajalt.mordant.widgets.Panel
 
-class ArgparseLocalization : Localization {
-    override fun usageTitle(): String = "usage:"
-    override fun optionsTitle(): String = "optional arguments:"
-    override fun argumentsTitle(): String = "positional arguments:"
-}
 
-class ArgparseHelpFormatter : CliktHelpFormatter(ArgparseLocalization()) {
-    override fun formatHelp(
-        localization: Localization,
-        error: UsageError?,
-        prolog: String,
-        epilog: String,
+class PanelHelpFormatter : MordantHelpFormatter() {
+    override fun renderParameters(
+        context: Context,
         parameters: List<HelpFormatter.ParameterHelp>,
-        programName: String,
-    ) = buildString {
-        // argparse prints arguments before options
-        addUsage(parameters, programName)
-        addProlog(prolog)
-        addArguments(parameters)
-        addOptions(parameters)
-        addCommands(parameters)
-        addEpilog(epilog)
+    ): Widget = verticalLayout {
+        width = ColumnWidth.Expand()
+        for (section in collectParameterSections(context, parameters)) {
+            cell(
+                Panel(
+                    section.content,
+                    section.title,
+                    expand = true,
+                    titleAlign = TextAlign.LEFT,
+                    borderStyle = context.terminal.theme.style("muted")
+                )
+            )
+        }
+    }
+
+    override fun styleSectionTitle(context: Context, title: String): String {
+        return context.terminal.theme.style("muted")(title)
     }
 }
 
 class Echo : CliktCommand(help = "Echo the STRING(s) to standard output") {
     init {
-        context { helpFormatter = ArgparseHelpFormatter() }
+        context { helpFormatter = PanelHelpFormatter() }
     }
 
     val suppressNewline by option("-n", help = "do not output the trailing newline").flag()

b87775d354633e9e55e58ebc0cb57cb55ab09cb0
AJ <aj@alt>
2023-11-01 11:44:13 -0700

    Update to Kotlin 1.9.20

--- clikt/build.gradle.kts
+++ clikt/build.gradle.kts
@@ -1,6 +1,5 @@
 @file:Suppress("UNUSED_VARIABLE", "KotlinRedundantDiagnosticSuppress")
 
-
 plugins {
     kotlin("multiplatform")
     alias(libs.plugins.publish)
@@ -37,44 +36,6 @@ kotlin {
                 api(libs.systemrules)
                 api(libs.jimfs)
             }
-        }
-
-        val nativeMain by creating {
-            dependsOn(commonMain)
-        }
-        val linuxX64Main by getting {
-            dependsOn(nativeMain)
-        }
-        val mingwX64Main by getting {
-            dependsOn(nativeMain)
-        }
-        val nativeDarwinMain by creating {
-            dependsOn(nativeMain)
-        }
-        val macosX64Main by getting {
-            dependsOn(nativeDarwinMain)
-        }
-        val macosArm64Main by getting {
-            dependsOn(nativeDarwinMain)
-        }
-
-        val nativeTest by creating {
-            dependsOn(commonTest)
-        }
-        val linuxX64Test by getting {
-            dependsOn(nativeTest)
-        }
-        val mingwX64Test by getting {
-            dependsOn(nativeTest)
-        }
-        val nativeDarwinTest by creating {
-            dependsOn(nativeTest)
-        }
-        val macosX64Test by getting {
-            dependsOn(nativeDarwinTest)
-        }
-        val macosArm64Test by getting {
-            dependsOn(nativeDarwinTest)
         }
     }
 }

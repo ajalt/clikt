bd8f6869abd5131d83ca03bb3593af93d4547250
AJ <aj@alt>
2024-07-14 09:57:32 -0700

    Add CoreNoOpCliktCommand

---
+++ clikt/src/commonMain/kotlin/com/github/ajalt/clikt/core/CoreNoOpCliktCommand.kt
@@ -0,0 +1,14 @@
+package com.github.ajalt.clikt.core
+
+/**
+ * A [CoreCliktCommand] that has a default implementation of [CoreCliktCommand.run] that is a no-op.
+ */
+open class CoreNoOpCliktCommand(
+    /**
+     * The name of the program to use in the help output. If not given, it is inferred from the
+     * class name.
+     */
+    name: String? = null,
+) : CoreCliktCommand(name) {
+    final override fun run() {}
+}

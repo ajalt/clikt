015ee1d5cc962a2031953880ecc7061502eda45a
AJ Alt <ajalt@users.noreply.github.com>
2024-07-20 08:13:06 -0700

    Don't print help on empty args when envvars are present (#532)

--- clikt-mordant/src/commonTest/kotlin/com/github/ajalt/clikt/testing/utils.kt
+++ clikt-mordant/src/commonTest/kotlin/com/github/ajalt/clikt/testing/utils.kt
@@ -1,6 +1,8 @@
 package com.github.ajalt.clikt.testing
 
+import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.UsageError
+import com.github.ajalt.clikt.core.context
 import com.github.ajalt.clikt.output.Localization
 import com.github.ajalt.clikt.output.ParameterFormatter
 
@@ -11,3 +13,7 @@ val Throwable.formattedMessage: String?
     ) ?: message
 
 internal val defaultLocalization = object : Localization {}
+
+internal fun <T : CliktCommand> T.withEnv(vararg entries: Pair<String, String?>): T {
+    return context { readEnvvar = { entries.toMap()[it] } }
+}
